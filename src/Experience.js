47a6e3bac124add6862a82beef5b079164958276
DMP92 <devin.pfromm.20@gmail.com>
2023-01-05 09:42:17 -0500

    first commit

---
+++ src/Experience.js
@@ -0,0 +1,75 @@
+import { Sparkles, Text, Html, ContactShadows, PresentationControls, Float, Environment, useGLTF } from '@react-three/drei'
+import * as THREE from 'three'
+
+export default function Experience()
+{
+    const macbook = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
+
+    return <>
+        <Environment preset="city" />
+
+        <color args={ [ '#615b69' ] } attach="background" />
+
+        {/* Controls Where User Can Move Screen */}
+        <PresentationControls
+            global
+            rotation={ [ 0.13, 0.1, 0] }
+            polar={ [ -0.4, 0.2 ] }
+            azimuth={ [ -1, 0.75 ] }
+            config={ { mass: 2, tension: 400 } }
+            snap={ { mass: 4, tension: 400 } }
+        >
+            <Float
+                rotationIntensity={ 0.4 }
+            >
+                {/* Blue Light Reflecting Off Keys */}
+                <rectAreaLight 
+                    width={ 2.5 }
+                    height={ 1.65 }
+                    intensity={ 65 } 
+                    color={ '#273bbd' }
+                    rotation={ [ 0.1, Math.PI, 0 ] }
+                    position={ [ 0, 0.55, - 1.15 ] }
+                />
+
+
+                {/* Laptop */}
+                <primitive 
+                    object={ macbook.scene } 
+                    position-y={ -1.2 }
+                >
+                    <Html
+                        transform
+                        wrapperClass='htmlScreen'
+                        distanceFactor={ 1.17 }
+                        position={ [ 0, 1.56, - 1.4 ] }
+                        rotation-x={ - 0.256 }
+                    > 
+                        <iframe src="https://www.spirradigital.com" />
+                    </Html>
+                </primitive>
+
+                <Text 
+                    font={"./ClashDisplay-Semibold.woff"}
+                    fontSize={ 0.8 }
+                    position={ [ 2, 0.75, 0.75 ] }
+                    rotation-y={ - 1.25 }
+                    maxWidth={ 1.5 }
+                    textAlign="center"
+                    color={ "white" }
+                >
+                    SPIRRA DIGITAL
+                </Text>
+                
+            </Float>
+        </PresentationControls>
+        />
+        
+        <ContactShadows 
+            position-y={ - 1.4 }
+            opacity={ 0.4 }
+            scale={ 5 }
+            blur={ 2.4 }
+        />
+    </>
+}
